6055360a2f0a51f7bc73a1e5da5a6ecac179339f
Sergi Carol <Sergi Carol@localhost>
2016-11-14 15:50:09 +0100

    Basic functionality implemented

--- __init__.py
+++ __init__.py
@@ -1,7 +1,11 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from .purchase import *
+
 
 def register():
     Pool.register(
+        PurchaseLine,
+        PurchasePromotion,
         module='purchase_promotion', type_='model')
---
+++ purchase.py
@@ -0,0 +1,85 @@
+#This file is part mifarma module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
+
+from trytond.model import ModelView, ModelSQL, MatchMixin, fields
+from trytond.pool import Pool, PoolMeta
+from trytond.pyson import Eval, If
+from trytond.transaction import Transaction
+
+__all__ = ['PurchaseLine', 'PurchasePromotion']
+__metaclass__ = PoolMeta
+
+
+class PurchaseLine:
+    __name__ = 'purchase.line'
+
+    promotion = fields.Char('Promotion', states={
+        'readonly': True,
+        })
+
+    @fields.depends('product')
+    def on_change_product(self):
+        print "Product is changing"
+        pool = Pool()
+        Promotion = pool.get('purchase.promotion')
+        print "Purchase", self.product
+        promotion = Promotion.get_promotions(self)
+        self.promotion = promotion.rec_name
+
+
+class PurchasePromotion(ModelSQL, ModelView, MatchMixin):
+    'Promotions for purchases'
+    __name__ = 'purchase.promotion'
+    _rec_name = 'promotion'
+
+    supplier = fields.Many2One('party.party', 'Supplier')
+    sequence = fields.Integer('Sequence')
+    product = fields.Many2One('product.product', 'Product')
+    promotion = fields.Char('Promotion', required=True)
+    company = fields.Many2One('company.company', 'Company', required=True,
+        domain=[
+            ('id', If(Eval('context', {}).contains('company'), '=', '!='),
+                Eval('context', {}).get('company', -1)),
+        ])
+
+    @classmethod
+    def __setup__(cls):
+        super(PurchasePromotion, cls).__setup__()
+        cls._order = [('sequence', 'ASC')]
+
+    @staticmethod
+    def default_company():
+        return Transaction().context.get('company')
+
+    @classmethod
+    def get_promotions(cls, purchase, pattern=None):
+        current_company = Transaction().context.get('company')
+        print current_company
+        promotions = cls.search([
+            ('product', '=', purchase.product.id),
+            ('company', '=', current_company)
+            ])
+        print "Promotions", promotions
+        if pattern == None:
+            pattern = {}
+        pattern = pattern.copy()
+        for promotion in promotions:
+            pattern.update(promotion.get_pattern(purchase))
+            if promotion.match(pattern):
+                return promotion
+
+    def get_pattern(self, purchase):
+        pattern = {}
+        if not self.product:
+            return pattern
+        pattern['product'] = purchase.product
+        return pattern
+
+    def match(self, pattern):
+        print pattern
+        if 'product' in pattern:
+            pattern = pattern.copy()
+            if self.product != pattern.pop('product'):
+                return False
+        return super(PurchasePromotion, self).match(pattern)
